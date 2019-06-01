5d5aa2c174606ace31003472bd92f3f9d5e4f8d2
liuw <simpliw@163.com>
2019-06-01 17:52:22 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
+    <HelloWorld />
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { Button } from "vant";
+import { Tabbar, TabbarItem } from "vant";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    "van-button": Button,
+    "van-tabbar": Tabbar,
+    "van-tabbar-item": TabbarItem
   }
 };
 </script>
