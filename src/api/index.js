dfdf001cec36861f4a930061ec10d0b46a31e8db
liuw <simpliw@163.com>
2019-06-03 23:40:50 +0800

    add interceptors

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,10 @@ const instance = axios.create({
   baseURL: "https://www.easy-mock.com/mock/5cf53d13a48c5b4da964d4f2/",
   timeout: 1000
 });
-
+instance.interceptors.response.use(
+  res => res.data,
+  err => {
+    console.info(err);
+  }
+);
 export const getList = () => instance.get("list");
