01c6436310f00e01562bb70d1383cd5d9c609ca1
liuw <simpliw@163.com>
2019-06-05 01:24:00 +0800

    edit vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import learningTasks from "./modules/learningTasks";
 import examAssess from "./modules/examAssess";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    learningTasks,
     examAssess
   }
 });
