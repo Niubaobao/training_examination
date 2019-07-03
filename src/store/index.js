a9141475d2ca95767f36ff0adfcbb5de2f61ef4c
liuw <simpliw@163.com>
2019-07-04 00:11:20 +0800

    考试详情

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import examAssess from "./modules/examAssess";
+import examAssessDetail from "./modules/examAssessDetail";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    examAssess
+    examAssess,
+    examAssessDetail
   }
 });
